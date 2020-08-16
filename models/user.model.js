a1a348cb514a7e0b4486ce5e8f5292684ba55f03
Fredy Somy <65971072+fredysomy@users.noreply.github.com>
2020-08-16 10:26:25 +0530

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -3,10 +3,10 @@ var mon=require('mongoose');
 var schema=mon.Schema;
 
 var userschema= new schema({
-  realname:{type:String,require:true},
-  name: {type:String,require:true,unique:true},
-  email:{type:String,require:true,unique:true},
-  pass:{type:String,require:true,unique:true},
+  realname:{type:String,require:true,trim:true},
+  name: {type:String,require:true,unique:true,trim:true},
+ email:{type:String,require:true,unique:true,trim:true},
+  pass:{type:String,require:true,unique:true,trim:true},
   verify:{type:Boolean,default:false}
 });
 
